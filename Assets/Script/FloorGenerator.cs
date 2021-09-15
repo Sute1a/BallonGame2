9e9992d89cd2a428dd161961ce54aadc90359bb4
Sute1a <yuki.t.415415@gmail.com>
2021-09-15 21:29:48 +0900

    手順１２

---
+++ Assets/Script/FloorGenerator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject aerialFloorPrefab;
+
+    [SerializeField]
+    private Transform generateTran;
+
+    [Header("生成までの待機時間")]
+    public float waitTime;
+    private float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= waitTime)
+        {
+            timer = 0;
+
+            GenerateFloor();
+        }
+    }
+
+    private void GenerateFloor()
+    {
+        GameObject obj = Instantiate(aerialFloorPrefab, generateTran);
+
+        float randomPoy = Random.Range(-4.0f, 4.0f);
+
+        obj.transform.position = new Vector2(obj.transform.position.x,
+            obj.transform.position.y + randomPoy);
+    }
+}
